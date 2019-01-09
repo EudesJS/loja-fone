7ec3fa760c50f026af42731ed28d0998cc395392
EudesJS <eudes.silva2013@gmail.com>
2019-01-09 02:14:00 +0000

    Solucionado o desafio avancado da aula 013

---
+++ Desafios aula 013/avancado.md
@@ -0,0 +1,5 @@
+# Desafio Avançado Collabcode aula 013
+
+- Descrição:
+
+    - Pegar o elemento com o texto "Carrinho (10)" e ao clicar no elemento com o texto "Adiciona no carrinho", assegure que o evento de clique altere o texto do primeiro elemento para "ae".
--- js/heart-image.js
+++ js/heart-image.js
@@ -6,18 +6,21 @@
 5 - No momento que o usuário clicar nós queremos mostar um AE => OK
 */
 
-const $heart = window.document.querySelector(".heart");
+/*const $heart = window.document.querySelector(".heart");
 
 $heart.addEventListener("click", handleClick);
 
 function handleClick() {
   console.log("ae");
-}
+}*/
 
 const $secondButton = window.document.querySelector(".button-store.-second");
+const $buy = window.document.querySelector(".action.-buy");
 
 $secondButton.addEventListener("click", handleClick);
 
 function handleClick() {
-  console.log("ae");
+  
+  $buy.innerHTML = "ae";
+  
 }
