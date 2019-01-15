b9632700b54b59254a010fcd6cbedc27beb03f80
EudesJS <eudes.silva2013@gmail.com>
2019-01-15 00:39:35 +0000

    Solucionado o desafio basico da aula 17

---
+++ Desafios aula 017/Desafio basico.md
@@ -0,0 +1,4 @@
+# Desafio Básico Collabcode aula 017
+
+- Descrição:
+    - Inserir e posicionar o ícone que irá representar a Busca.
---
+++ js/search.js
@@ -0,0 +1,3 @@
+const $search = document.querySelector("input.search");
+
+$search.addEventListener("click", () => {$search.classList.toggle("-active");}); //arrow function 
