f9dc488bc823b7f5bfddeb07caa331c2668aa32a
EudesJS <eudes.silva2013@gmail.com>
2019-01-23 05:20:55 +0000

    Solucionado o desafio avancado aula 017

---
+++ Desafios aula 017/avancado.md
@@ -0,0 +1,4 @@
+# Desafio avançado Collabcode aula 017
+
+- Descrição:
+    - Animar o elemento da busca com Javascript. Ao clicar na lupa, abre o campo busca e ao clicar no X fecha o campo busca.
--- js/search.js
+++ js/search.js
@@ -1,6 +1,11 @@
 const $search = document.querySelector(".search");
 const $searchIcon = document.querySelector(".search-icon");
-$searchIcon.addEventListener("click", () => {$search.classList.toggle("-active");$searchIcon.classList.toggle("-active");});//arrow function 
+const $closeIcon = document.querySelector(".close-icon");
+
+$searchIcon.addEventListener("click", () => {$search.classList.add("-active");$searchIcon.classList.remove("-active");$closeIcon.classList.add("-active")});//arrow function 
+
+$closeIcon.addEventListener("click", () => {$search.classList.remove("-active");
+$closeIcon.classList.remove("-active");$searchIcon.classList.add("-active");})
 
 
 
